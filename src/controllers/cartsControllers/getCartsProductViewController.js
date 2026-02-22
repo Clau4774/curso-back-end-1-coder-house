ba246057775021773dcb885f8da21e3583e0866f
claudioDang47 <claudiodangiolo@gmail.com>
2026-02-22 19:40:29 -0300

    Se creo controlador para la vista del carro

---
+++ src/controllers/cartsControllers/getCartsProductViewController.js
@@ -0,0 +1,44 @@
+import { CartsManager } from "../../cartsManager/CartsManager.js";
+import { sendResponse } from "../../utils/sendResponse.js";
+
+export const getCartsProductViewController = async (req, res) => {
+    try {
+        const {cid} = req.params;
+        const cartManager = new CartsManager();
+        const data = await cartManager.getCartAndPopulate(cid);
+
+        if(!data) {
+            throw data;
+        }
+
+        const {payload} = data;
+
+        const {products} = payload;
+
+        const productsWithTotal = products.map(product => {
+            return {
+                ...product,
+                totalPrice: product.productId.price * product.quantity
+            }
+        });
+
+        const cartTotalPrice = productsWithTotal.reduce((acc, curr) => acc += curr.totalPrice, 0);
+
+        const fixedPrices = productsWithTotal.map(product => {
+                return {
+                    ...product,
+                    totalPrice: product.totalPrice.toFixed(2)
+                }
+            })
+
+        const cartProductsWithTotal = {
+            cartProducts: [...fixedPrices],
+            cartTotalPrice: cartTotalPrice.toFixed(2)
+        }      
+
+        res.render('cartView', {cartProductsWithTotal});
+
+    } catch (error) {
+        sendResponse(error, res);
+    }
+}
--- src/routes/cartRouter.js
+++ src/routes/cartRouter.js
@@ -1,48 +1,7 @@
 import { Router } from "express";
-import { CartsManager } from "../cartsManager/CartsManager.js";
-import { sendResponse } from "../utils/sendResponse.js";
+import { getCartsProductViewController } from "../controllers/cartsControllers/getCartsProductViewController.js";
 
 
 export const cartRouter = Router();
 
-cartRouter.get('/:cid', async (req, res) => {
-    try {
-        const {cid} = req.params;
-        const cartManager = new CartsManager();
-        const data = await cartManager.getCartAndPopulate(cid);
-
-        if(!data) {
-            throw data;
-        }
-
-        const {payload} = data;
-
-        const {products} = payload;
-
-        const productsWithTotal = products.map(product => {
-            return {
-                ...product,
-                totalPrice: product.productId.price * product.quantity
-            }
-        });
-
-        const cartTotalPrice = productsWithTotal.reduce((acc, curr) => acc += curr.totalPrice, 0);
-
-        const fixedPrices = productsWithTotal.map(product => {
-                return {
-                    ...product,
-                    totalPrice: product.totalPrice.toFixed(2)
-                }
-            })
-
-        const cartProductsWithTotal = {
-            cartProducts: [...fixedPrices],
-            cartTotalPrice: cartTotalPrice.toFixed(2)
-        }      
-
-        res.render('cartView', {cartProductsWithTotal});
-
-    } catch (error) {
-        sendResponse(error, res);
-    }
-})
+cartRouter.get('/:cid', getCartsProductViewController)
